1ae4c7c9b87793f661876179bc8e990070687d46
ceo_of_github <142130973+ceo-of-git@users.noreply.github.com>
2026-05-23 15:01:51 -0400

    Event Handler Organization
    +90000 aura

---
+++ src/main/java/xyz/nasasupercomputer/birmingham/EventHandlers/Client/ClientEventHandler.java
@@ -0,0 +1,41 @@
+package xyz.nasasupercomputer.birmingham.EventHandlers.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import xyz.nasasupercomputer.birmingham.ForgeConfigs;
+import xyz.nasasupercomputer.birmingham.MainRegistry;
+import xyz.nasasupercomputer.birmingham.ItemHazards.HazardSystem;
+
+@Mod.EventBusSubscriber(modid = MainRegistry.MOD_ID)
+public class ClientEventHandler {
+
+	@SubscribeEvent
+	public static void drawTooltip(ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        List<Component> tooltipList = event.getToolTip();
+        List<String> tooltipString = new ArrayList<String>();
+        
+        // ===================
+        // ITEM HAZARDS
+        if (ForgeConfigs.enableItemHazards) {
+	        
+	        // Apply Hazard Description
+	        HazardSystem.ApplyHazardTooltip(stack, event.getEntity(), tooltipString);
+	        
+	        // Finalize any changes. by converting & adding back onto tooltipString back to tooltipList components.
+	        for (String line : tooltipString) {
+	            tooltipList.add(Component.literal(line));
+	        }
+        }
+	}
+}
--- src/main/java/xyz/nasasupercomputer/birmingham/EventHandlers/Client/ServerEventHandler.java
+++ src/main/java/xyz/nasasupercomputer/birmingham/EventHandlers/Client/ServerEventHandler.java
@@ -1,23 +1,21 @@
-package xyz.nasasupercomputer.birmingham.ItemHazards;
+package xyz.nasasupercomputer.birmingham.EventHandlers.Client;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import xyz.nasasupercomputer.birmingham.ForgeConfigs;
 import xyz.nasasupercomputer.birmingham.MainRegistry;
+import xyz.nasasupercomputer.birmingham.ItemHazards.HazardSystem;
 
 @Mod.EventBusSubscriber(modid = MainRegistry.MOD_ID)
-public class HazardEventHandler {
-
+public class ServerEventHandler {
+	
 	@SubscribeEvent
 	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 		
@@ -42,25 +40,6 @@ public class HazardEventHandler {
 		    for (ItemStack stack : inventoryItems) {
 		        HazardSystem.ApplyHazard(stack, player);
 		    }
-        }
-	}
-	
-
-	@SubscribeEvent
-	public static void drawTooltip(ItemTooltipEvent event) {
-        ItemStack stack = event.getItemStack();
-        List<Component> tooltipList = event.getToolTip();
-        List<String> tooltipString = new ArrayList<String>();
-        
-        if (ForgeConfigs.enableItemHazards) {
-	        
-	        // Apply Hazard Description
-	        HazardSystem.ApplyHazardTooltip(stack, event.getEntity(), tooltipString);
-	        
-	        // Finalize any changes. by converting & adding back onto tooltipString back to tooltipList components.
-	        for (String line : tooltipString) {
-	            tooltipList.add(Component.literal(line));
-	        }
         }
 	}
 }
