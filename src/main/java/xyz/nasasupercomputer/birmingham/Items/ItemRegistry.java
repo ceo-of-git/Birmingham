203d7c75dfd4c78017de5573be1c6daeb7b9684c
Vinxinty <40676599+TheMaster-Git@users.noreply.github.com>
2026-05-23 23:46:02 -0400

    ehenery drinkl changes
    now it cna go in the curios slot. and notihng else. also added a file for curios that modify attributes but i do not knopw where to even begin on finishing that

--- src/main/java/xyz/nasasupercomputer/birmingham/Items/ItemRegistry.java
+++ src/main/java/xyz/nasasupercomputer/birmingham/Items/ItemRegistry.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import xyz.nasasupercomputer.birmingham.Items.custom.attributeCurios;
 import xyz.nasasupercomputer.birmingham.MainRegistry;
 import xyz.nasasupercomputer.birmingham.Items.custom.Gloves;
 
@@ -17,11 +18,13 @@ public class ItemRegistry {
 	// =========================
 	// ORIGINAL ITEMS
 	public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item", () -> new Item(new Item.Properties().stacksTo(64)));
-	public static final RegistryObject<Item> ENERGY_DRINK = ITEMS.register("energy_drink", () -> new Item(new Item.Properties().stacksTo(1).food(new FoodProperties.Builder().nutrition(4).saturationMod(0.6f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1600, 4), 1f).alwaysEat().build())));
 	public static final RegistryObject<Item> RAW_ADAMANTITE = ITEMS.register("raw_adamantite", () -> new Item(new Item.Properties().stacksTo(64)));
 	
 	// =========================
 	// TOOLS / CURIOS
 	public static final RegistryObject<Item> LEATHER_GLOVES = ITEMS.register("gloves_leather", () -> new Gloves(new Item.Properties().stacksTo(1), 1, null));
 	public static final RegistryObject<Item> CREATIVE_GLOVES = ITEMS.register("gloves_creative", () -> new Gloves(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC), 69420, null));
+
+	public static final RegistryObject<Item> ENERGY_DRINK = ITEMS.register("energy_drink", () -> new Item(new Item.Properties().stacksTo(1).food(new FoodProperties.Builder().nutrition(4).saturationMod(0.6f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1600, 4), 1f).alwaysEat().build())));
+
 }
