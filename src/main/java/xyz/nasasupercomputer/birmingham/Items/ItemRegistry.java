696707e94d61c4b949b46ab3eccc426f69f0f360
Vinxinty <40676599+TheMaster-Git@users.noreply.github.com>
2026-05-24 15:04:11 -0400

    energy drink works
    woohoo

--- src/main/java/xyz/nasasupercomputer/birmingham/Items/ItemRegistry.java
+++ src/main/java/xyz/nasasupercomputer/birmingham/Items/ItemRegistry.java
@@ -8,7 +8,7 @@ import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import xyz.nasasupercomputer.birmingham.Items.custom.attributeCurios;
+import xyz.nasasupercomputer.birmingham.Items.curios.energyDrink;
 import xyz.nasasupercomputer.birmingham.MainRegistry;
 import xyz.nasasupercomputer.birmingham.Items.custom.Gloves;
 
@@ -25,6 +25,6 @@ public class ItemRegistry {
 	public static final RegistryObject<Item> LEATHER_GLOVES = ITEMS.register("gloves_leather", () -> new Gloves(new Item.Properties().stacksTo(1), 1, null));
 	public static final RegistryObject<Item> CREATIVE_GLOVES = ITEMS.register("gloves_creative", () -> new Gloves(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC), 69420, null));
 
-	public static final RegistryObject<Item> ENERGY_DRINK = ITEMS.register("energy_drink", () -> new Item(new Item.Properties().stacksTo(1).food(new FoodProperties.Builder().nutrition(4).saturationMod(0.6f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1600, 4), 1f).alwaysEat().build())));
+	public static final RegistryObject<Item> ENERGY_DRINK = ITEMS.register("energy_drink", () -> new energyDrink(new Item.Properties().stacksTo(1).food(new FoodProperties.Builder().nutrition(4).saturationMod(0.6f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1600, 4), 1f).alwaysEat().build()), 1f));
 
 }
---
+++ src/main/java/xyz/nasasupercomputer/birmingham/Items/curios/energyDrink.java
@@ -0,0 +1,114 @@
+package xyz.nasasupercomputer.birmingham.Items.curios;
+
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import com.mojang.logging.LogUtils;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import org.slf4j.Logger;
+import org.w3c.dom.Attr;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import java.util.UUID;
+
+// okl im just straight up not doing it this way, im sure 1 for every item will be fiiiiiiiiiiine
+
+/* TODO :
+
+
+* - Literally everything
+* - kill themaster
+* - world domination
+*
+* */
+
+
+
+
+public class energyDrink extends Item implements ICurioItem {
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    float level;
+
+    public energyDrink(Properties pProperties, float level) {
+        super(pProperties);
+
+        this.level = level;
+    }
+
+    // Appends a custom description to the item. than ks coe fore code
+//    @Override
+//    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag ) {
+//        tooltip.add(Component.translatable("tooltip.birmingham.gloves"));
+//        tooltip.add(Component.translatable("tooltip.birmingham.protects_from"));
+//
+//        super.appendHoverText(stack, level, tooltip, flag);
+//    }
+    // cant be bothered to do that rn, but ill do it later (n o i wont)
+
+
+
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        // ticking logic here
+//        LivingEntity player = slotContext.entity();
+//        AttributeInstance attribute = player.getAttribute(Attributes.MOVEMENT_SPEED);
+//        AttributeModifier modifier = new AttributeModifier( // ignore my ass formatting
+//            modifierUUID, // still dont know why this is needed
+//          "speed", // internal name (?)
+//        0.5 * level, //amount
+//        AttributeModifier.Operation.MULTIPLY_TOTAL // self-explanatory
+//        );
+//        if (attribute != null && !attribute.hasModifier(modifier)) {
+//            attribute.addPermanentModifier(modifier);
+//        }
+
+        }
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack)
+    {
+        Multimap<Attribute, AttributeModifier> map = LinkedHashMultimap.create(); // code ENTIRELY stolen, i do not understnad a bit of it
+        map.put(
+                Attributes.MOVEMENT_SPEED,
+                new AttributeModifier(uuid, "speed", 0.5 * level, AttributeModifier.Operation.MULTIPLY_TOTAL)
+        );
+        return map;
+
+    }
+
+    // has to be false since its eatable as well
+    public boolean canEquipFromUse(SlotContext slotContext, ItemStack stack) {
+        return false;
+    }
+
+
+    // thanks google overview
+    // Changes the holding animation to the drinking animation (like a potion)
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack) {
+        return UseAnim.DRINK;
+    }
+
+    // Overrides the default eating sound to the generic drinking sound
+    @Override
+    public SoundEvent getDrinkingSound() {
+        return SoundEvents.GENERIC_DRINK;
+    }
+
+    // Overrides the sound that plays when the drinking finishes
+    @Override
+    public SoundEvent getEatingSound() {
+        return SoundEvents.GENERIC_DRINK;
+    }
+
+
+}
